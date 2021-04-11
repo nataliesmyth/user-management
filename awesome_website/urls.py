1a4dc1578ce3da186df0152a21133e36143e4470
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-11 13:43:40 -0400

    added apps urls to project urls

--- awesome_website/urls.py
+++ awesome_website/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# awesome_website/urls.py
+
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r"^", include("users.urls")),
+    url(r"^admin/", admin.site.urls),
 ]
