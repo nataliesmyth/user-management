05312e3326263ef9e613c6cfec3535a770032570
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-11 13:31:00 -0400

    updated template path

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/natalie/django/user_management/venv"
+set -gx VIRTUAL_ENV "/Users/natalie/django/awesome_website/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
