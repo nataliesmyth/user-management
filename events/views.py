0bd34293a42b22395d9e3486974f34d702ea427d
Nataliesmyth <nataliesmyth11@gmail.com>
2021-10-16 13:22:46 -0400

    dynamic urls

--- events/views.py
+++ events/views.py
@@ -4,15 +4,23 @@ from calendar import HTMLCalendar
 
 # Create your views here.
 def home(request, year, month):
-    # the curly brackets below is the context dictionary, which allows us to pass data from the backend to the front end of your website
     name = 'Natalie'
     month = month.title()
+
     # Convert month from name to number
     month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # Create a calendar
+
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
     return render(request, 
+    # the curly brackets below is the context dictionary, which allows us to pass data from the backend to the front end of your website
     'home.html', {
         'name': name,
         'year': year,
         'month': month,
         'month_number': month_number,
+        'cal': cal,
     })
