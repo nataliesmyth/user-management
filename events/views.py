c916afea37c85f9b5da27ccea47183d787aa6ca1
Nataliesmyth <nataliesmyth11@gmail.com>
2021-10-17 14:14:33 -0400

    added current time

--- events/views.py
+++ events/views.py
@@ -13,8 +13,14 @@ def home(request, year, month):
     month_number = int(month_number)
 
     # Create a calendar
-
     cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # Get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # Get current time
+    time = now.strftime('%I:%M %p')
 
     return render(request, 
     # the curly brackets below is the context dictionary, which allows us to pass data from the backend to the front end of your website
@@ -24,4 +30,6 @@ def home(request, year, month):
         'month': month,
         'month_number': month_number,
         'cal': cal,
+        'current_year': current_year,
+        'time': time,
     })
